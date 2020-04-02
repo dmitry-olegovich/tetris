fff4a5169f50b579208a3b5e0e6b48aaeb4afd1e
Dmitry Kolomytkin <dmitkolom@gmail.com>
2020-04-02 12:50:48 +0300

    Feature: static cell field class added, continue with Figure class

---
+++ modules/StaticObjects.py
@@ -0,0 +1,111 @@
+""" READ ME
+This module descirbes the basic classes for static objects used 
+in tetris game, such as:
+- screen boundary,
+- fallen static cells.
+"""
+
+from TetrisObjects import Figure
+
+class Field():
+    """Describes the fallen static cells.
+    
+    Args:
+        max_x (int): horisontal size
+        max_y (int): vertical size
+    
+    Attributes:
+        _array (list): 2D list; 0 = empty, non-0 = color of cell
+    """
+
+    def __init__(self, max_x: int = 10, max_y: int = 20):
+        self._array = [list([0] * max_x) for Y in range(max_y)]
+
+    def __repr__(self):
+        result = ''
+        for line in self._array:
+            for elem in line:
+                if elem == 0:
+                    result += '_'
+                else:
+                    result += 'X'
+            result += '\n'
+        return result
+
+    @property
+    def width(self):
+        return len(self._array[0])
+
+    @property
+    def height(self):
+        return len(self._array)
+
+    def grow(self, figure):
+        """Add figure to field of fallen cells."""
+
+        for cell in figure.cells:
+            self._array[cell.y][cell.x] = figure.color
+
+    def check_down(self, figure):
+        """Check if figure can go down."""
+        
+        for cell in figure.cells:
+            if cell.y + 1 >= self.height:
+                return False
+            if self._array[cell.y + 1][cell.x]:
+                return False
+        
+        return True
+
+    def check_left(self, figure):
+        """Check if figure can go left."""
+
+        for cell in figure.cells:
+            if cell.x - 1 < 0:
+                return False
+            if self._array[cell.y][cell.x - 1]:
+                return False
+        
+        return True
+
+    def check_right(self, figure):
+        """Check if figure can go right."""
+
+        for cell in figure.cells:
+            if cell.x + 1 >= self.width:
+                return False
+            if self._array[cell.y][cell.x + 1]:
+                return False
+        
+        return True
+
+    def check_full_lines(self):
+        """Return list of line numbers that are full."""
+        
+        result = []
+
+        for i in range(self.height):
+            if self.check_full_line(i):
+                result.append(i)
+        
+        return result
+
+    def check_full_line(self, line_num):
+        """Return True if line is full."""
+        
+        if 0 in self._array[line_num]:
+            return False
+        return True
+
+if __name__ == "__main__":
+    field = Field()
+    fig = Figure(0, 5, 19)
+    field.grow(fig)
+    print(field, end='\n\n')
+    fig = Figure(0, 6, 6)
+    fig.rotate()
+    while field.check_down(fig):
+        fig.drop()
+    field.grow(fig)
+    print(field)
+    
--- modules/TetrisObjects.py
+++ modules/TetrisObjects.py
@@ -2,6 +2,9 @@
 This module descirbes the basic classes for objects used in tetris game.
 
 """
+from collections import namedtuple
+
+Coordinate = namedtuple('Coord', ['x', 'y'])
 
 class Cell():
     """Simple cell - building block of other objects.
@@ -17,32 +20,34 @@ class Cell():
         _color (int): this is used to store color number value.
         _static (bool): static cells do not move.
     """
-    def __init__(self, x:int, y:int, color:int):
-        self._pos = (x, y)
+    def __init__(self, x: int = 0, y: int = 0, color: int = 1):
+        self._pos = Coordinate(x=x, y=y)
         self._color = color
         self._static = False
     
     def __repr__(self):
         return f"({self.x}, {self.y})"
 
-    def rotation(self):
-        """Change position as if the cell is rotated 90 deg. clockwise
-        around the 0,0 point.
+    def rotate(self):
+        """Change position as if the cell is rotated 90 deg 
+        counter-clockwise around the 0,0 point.
         """
-        self._pos = (self._pos[1], -self._pos[0])
+        self._pos = Coordinate(self.y, -self.x)
     
+    # Not needed maybe?
     @property
     def x(self):
-        return self._pos[0]
-
+        return self._pos.x
+    
+    # Not needed maybe?
     @property
     def y(self):
-        return self._pos[1]
+        return self._pos.y
 
 
 
 class Figure():
-    """Tetris figure - "consist" of 'Cell' objects as building blocks.
+    """Tetris figure - consists of 'Cell' objects as building blocks.
     
     Args:
         figure_code (int): code that defines what the figure will be;
@@ -50,9 +55,10 @@ class Figure():
             '_FIGURES' class atribute.
 
     Atributes:
-        _pos (tuple): position of the center cell (that is at 0,0).
-        _cells (list): list of 'Cell' objects.
-        _color (int): color of the cells.
+        _pos (tuple): position of the center cell (that is at 0,0),
+        _cells (list): list of 'Cell' objects,
+        _color (int): color of the cells,
+        _dont_rotate (bool): as square blocks look wierd when rotated.
     """
     
     _FIGURES = [
@@ -65,14 +71,69 @@ class Figure():
         [(0,0), (1,0), (0,1), (1,1)],       # square
     ]
 
-    def __init__(self, figure_code):
+    def __init__(self, figure_code: int = 0, x: int = 0, y: int = 0):
         index = figure_code % len(self._FIGURES)
-        self._color = index
-        self._cells = [Cell(x, y, self._color) for x, y in self._FIGURES[index]]
-            
+        self._color = index + 1  # can never be 0
+        self._cells = [Cell(x, y, self._color) for x, y 
+            in self._FIGURES[index]]
+        self._pos = Coordinate(x, y)
+        if index = 6:
+            self._dont_rotate = True
+        else:
+            self._dont_rotate = False
+    
+    def rotate(self):
+        """Rotate figure 90 deg counter-clockwise. No safety check!"""
+        
+        if self._dont_rotate:
+            return
+
+        for cell in self._cells:
+            cell.rotate()
+
+    def left(self):
+        """Change postion of the figure left. No safety check!"""
+
+        self._pos = Coordinate(self._pos.x - 1, self._pos.y)
+
+    def right(self):
+        """Change postion of the figure right. No safety check!"""
+
+        self._pos = Coordinate(self._pos.x + 1, self._pos.y)
+
+    def drop(self):
+        """Change postion of the figure down. No safety check!"""
+
+        self._pos = Coordinate(self._pos.x, self._pos.y + 1)
+
+    @property
+    def cells(self):
+        """Return cells list with absolute coordinates."""
+
+        result = []
+        for cell in self._cells:
+            result.append(Cell(cell.x + self.x,
+                cell.y + self.y, self.color))
+
+        return result
+
+    @property
+    def color(self):
+        return self._color
+
+    @property
+    def x(self):
+        return self._pos.x
+
+    @property
+    def y(self):
+        return self._pos.y
+
+    # only used for debug purposed __repr__ 
     @property
     def x_range(self):
         """Return the min and max X coord of the cells."""
+
         min, max = 0, 0
         for cell in self._cells:
             if cell.x < min:
@@ -81,10 +142,12 @@ class Figure():
                 max = cell.x
         
         return min, max
-
+    
+    # only used for debug purposed __repr__ 
     @property
     def y_range(self):
         """Return the min and max Y coord of the cells."""
+
         min, max = 0, 0
         for cell in self._cells:
             if cell.y < min:
@@ -94,10 +157,11 @@ class Figure():
         
         return min, max
 
+    # only needed for debug purposes
     def __repr__(self):
         x_min, x_max = self.x_range
         y_min, y_max = self.y_range
-        line = '.' * (abs(x_max - x_min) + 1)
+        line = ' ' * (abs(x_max - x_min) + 1)
         text = [ list(line) for x in range((abs(y_max - y_min) + 1))]
         
         for cell in self._cells:
@@ -108,13 +172,17 @@ class Figure():
             for char in line:
                 result += char
             result += '\n'
-            
 
         return result
 
 if __name__ == "__main__":
-    for i in range(7):
+    """for i in range(7):
         obj = Figure(i)
+        print(obj, end="\n")
+        obj.rotate()
         print(obj, end="\n\n")
-   
-
+    """
+    obj = Figure(1)
+    print(obj, end="\n")
+    obj.rotate()
+    print(obj, end="\n\n")
